17cc7afd741757cf6cc83fb5d3fc39ed1acc0e2a
Виталий Рублевский <vitrub76@gmail.com>
2023-11-14 16:31:17 +0200

    Добавьте файлы проекта.

---
+++ DZ_06_11_23___MATRIX/DZ_06_11_23___MATRIX.cpp
@@ -0,0 +1,43 @@
+﻿
+#include "Matrix.h"
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <utility>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main()
+{
+    Matrix<int> matrix1(3, 3), matrix2(3, 3);
+    
+    cout << "Column: " << matrix1.getColumn() << endl;
+    cout << " Row:   " << matrix1.getRow() << endl;
+    cout << endl << endl;
+    cout << "Matrix 1" << endl;
+    matrix1.print();
+    cout << endl << endl;
+
+    Matrix<int> matrix3(matrix1);
+
+    cout << "Matrix 3" << endl;
+    matrix3.print();
+    cout << endl << endl;
+    cout << "Matrix 2" << endl;
+    matrix2.fillMatrixRand(3, 3);
+    matrix2.print();
+    cout << endl << endl;
+    cout << matrix2.getMatrixIJ(2, 1) << endl;
+    cout << endl << endl;
+
+    Matrix<int> matrix4(3, 3);
+    matrix4.fillMatrixHand(3, 3);
+    matrix4.print();
+
+    
+}
+
+
---
+++ DZ_06_11_23___MATRIX/Matrix.h
@@ -0,0 +1,111 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <utility>
+#include <iomanip>
+using namespace std;
+
+template<typename T> class Matrix
+{
+	T rows = 0;  
+	T columns = 0;  
+	T** matrix = nullptr;
+
+public:
+    Matrix() {}
+
+    Matrix( T _rows, T _columns) : rows(_rows), columns(_columns) {
+        matrix = (T**) new T*[rows];
+        for (int i = 0; i < rows; i++)
+            matrix[i] = (T*)new T[columns];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                matrix[i][j] = 8;
+       
+    };
+
+    Matrix(const Matrix& _matrix) {
+        rows = _matrix.rows;
+        columns = _matrix.columns;
+        matrix = (T**) new T * [rows]; 
+
+        for (int i = 0; i < rows; i++)
+            matrix[i] = (T*) new T[columns];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                matrix[i][j] = _matrix.matrix[i][j];
+    }
+
+    ~Matrix() {
+        for (int i = 0; i < rows; i++)
+            delete[] matrix[i];
+
+        delete[] matrix;
+    }
+    
+    void fillMatrixRand(int rows, int columns)
+    {
+        srand(time(0));
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                matrix[i][j] = rand() % 20 + 1;
+    }
+
+    void fillMatrixHand(int rows, int columns)
+    {
+        
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++) {
+                T x = NULL;
+                cout << "Enter  x ";
+                cin >> x;
+                matrix[i][j] = x;
+            }
+                
+    }
+
+    void print()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                cout << matrix[i][j] << " ";
+            }
+
+            cout << endl;
+        }
+    }
+
+    int getRow()
+    {
+        return rows;
+    };
+    int getColumn()
+    {
+        return columns;
+    };
+    T getMatrixIJ(int i, int j) {
+        if ((rows > 0) && (columns > 0))
+            return matrix[i][j];
+        else
+            return 0;
+    }
+    void SetMatrixIJ(int i, int j, T value)
+    {
+        if ((i < 0) || (i >= rows))
+            return;
+        if ((j < 0) || (j >= columns))
+            return;
+        matrix[i][j] = value;
+    }
+    
+
+
+    Matrix& operator+(Matrix&) {}
+   
+
+};
+
